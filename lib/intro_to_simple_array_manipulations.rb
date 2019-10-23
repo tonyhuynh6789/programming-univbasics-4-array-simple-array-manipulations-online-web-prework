d67ec1df9f0283bd55e54201f91c94ba4e73c71a
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-23 22:14:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,7 +46,7 @@ def using_delete(instructors,no_offense_steven)
 end 
 
 def using_delete_at(famous_robots,deleted_robo)
-  famous_robots.delete(deleted_robo)
+  famous_robots.delete("Robocop")
 end 
 
   
